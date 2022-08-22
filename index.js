c2e93c22ecb8209a386565394259314d62a384d3
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-08-22 12:23:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function addingEventListener() {
     btn.addEventListener("click",()=>{alert("Hello, I was clicked")})
 }
 addingEventListener()
+
+// const btn = document.getElementById('button')
+// btn.addEventListener('click', ()=> {alert("Hello, I was clicked")})
+// console.log
